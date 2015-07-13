4562550a0b5427d7355efcc19c88efb0050d6ed3
Zachary Nagler <zacharynagler@sysgrwjwhls-MacBook-Pro.local>
2015-07-13 15:46:05 -0400

    improve layout

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ window.onload = function() {
   d3.json(file, ready)
 
   Global.margin = margin = {
-    top: 20,
+    top: 10,
     right: 10,
     bottom: 40,
     left: 10
   };
 
   Global.width = width = 960 - margin.left - margin.right;
-  Global.height = height = 500 - margin.top - margin.bottom;
+  Global.height = height = 815 - margin.top - margin.bottom;
 
   // Global.svg = svg = d3.select(".d3-container").append("svg")
   //   .attr("class", "main-svg")
@@ -24,12 +24,11 @@ window.onload = function() {
   Global.xScale = d3.scale.linear()
     .range([0, width])
 
-  modifyBernieLink()
-  addRedditLink()
+
 }
 
 function modifyBernieLink(){
-  var svg = d3.select('.link-svg')
+  var svg = d3.select('.main-svg')
   d3.select('.bernie-link')
   .attr("transform","translate(100,50) scale(.6.6)")
 
@@ -46,7 +45,7 @@ function modifyBernieLink(){
 }
 
 function addRedditLink() {
-  var svg = d3.select('.link-svg')
+  var svg = d3.select('.main-svg')
 
   var text = svg.append("text")
     .attr("x", 450 + 280)
@@ -73,14 +72,29 @@ function ready(error, raw) {
   var margin = Global.margin
   var xScale = Global.xScale
 
+
   data = raw.map(function(o) {
     var newObj = o,
       month = o.date.split(" ")[0],
       year = o.date.split(" ")[1],
       dateValue = getMonthFromString(month) + ((year - 1981) * 12)
     newObj.dateValue = dateValue
+    newObj.blurb = newObj.date + " -- " + newObj.blurb
     return newObj
   })
+  data.forEach(function(o){
+    if (o.dateValue == 119) o.dateValue = 118
+    if (o.dateValue == 121) o.dateValue = 123
+    if (o.dateValue == 223) o.dateValue = 219
+    if (o.dateValue == 121) o.dateValue = 123
+    if (o.dateValue == 227) o.dateValue = 230
+    if (o.dateValue == 348) o.dateValue = 345
+    if (o.dateValue == 351) o.dateValue = 350
+    if (o.dateValue == 384) o.dateValue = 378
+    if (o.dateValue == 387) o.dateValue = 383
+    if (o.dateValue == 409) o.dateValue = 408
+  })
+  console.log(data.map(function(o){return o.dateValue}))
   console.log(data)
 
   Global.xScale
@@ -94,7 +108,7 @@ function ready(error, raw) {
   }).join(' ')
 
   var currentBlurb = -1
-  var lineData = d3.wordwrap(Global.allText, 150)
+  var lineData = d3.wordwrap(Global.allText, 130)
     .map(function(str) {
       var returnArray = str.split("~").map(function(str, i) {
         return [i + currentBlurb, str]
@@ -107,7 +121,7 @@ function ready(error, raw) {
   // var allText = allTextArray
   var text = svg.append('g.text-group')
     .append('text.all-text')
-    .attr("transform", "scale(1,.8)")
+    .attr("transform", "scale(1,1)")
 
   // .attr('opacity',.5)
 
@@ -136,24 +150,12 @@ function ready(error, raw) {
       return d[1]
     })
 
-  var timeline = {
-      y: 2 * (height / 3),
-      height: 15
-    }
+  var swishY = 370
+
+
     // Timeline
 
-  var swishString = "M480,146.5c0,0-1.8-0.4-5.1-1.1c-1.7-0.4-3.7-0.7-6.1-1.2c-2.4-0.4-5.2-0.9-8.3-1.4"
-  +    "c-6.2-1.1-13.8-2-22.5-3c-8.7-1-18.6-1.8-29.3-2.4c-10.7-0.6-22.4-1-34.7-1.3c-6.2-0.1-12.5-0.1-19-0.1c-6.5,0-13.1,0.1-19.8,0.2"
-  +    "c-13.4,0.3-27.3,0.7-41.4,1.5c-14.1,0.7-28.4,1.6-42.7,2.7c-7.2,0.6-14.3,1.2-21.4,1.8c-7.1,0.6-14.2,1.3-21.3,2"
-  +    "c-3.5,0.4-7,0.7-10.5,1.1c-3.5,0.4-6.9,0.7-10.4,1.1c-3.5,0.4-6.9,0.8-10.3,1.2c-3.4,0.4-6.8,0.8-10.2,1.1"
-  +    "c-13.5,1.3-26.6,2.3-39,2.7c-6.2,0.2-12.3,0.4-18.1,0.4c-5.9,0.1-11.5,0-16.9-0.1c-10.9-0.3-20.8-1-29.6-1.9"
-  +    "c-8.8-1.1-16.5-2.3-22.7-3.9c-3.1-0.8-5.9-1.6-8.3-2.4c-2.4-0.8-4.4-1.7-6-2.4c-1.6-0.8-2.8-1.4-3.5-1.9c-0.8-0.5-1.2-0.8-1.2-0.8"
-  +    "s0.4,0.2,1.3,0.5c0.8,0.3,2.1,0.7,3.7,1.2c1.6,0.4,3.7,0.9,6.1,1.2c2.4,0.4,5.2,0.7,8.3,1c3.1,0.2,6.6,0.4,10.3,0.5"
-  +    "c3.8,0.1,7.8,0.1,12.2,0.1c8.7-0.1,18.6-0.5,29.3-1.1c5.4-0.3,10.9-0.7,16.7-1.2c5.8-0.5,11.8-1,17.9-1.5"
-  +    "c12.3-1.1,25.2-2.5,38.6-4.1c6.7-0.8,13.5-1.6,20.4-2.5c6.9-0.8,14-1.6,21-2.2c7.1-0.7,14.2-1.3,21.4-1.9"
-  +    "c7.2-0.6,14.4-1.1,21.6-1.5c14.4-0.9,28.9-1.5,43.1-1.8c14.2-0.3,28.2-0.3,41.8,0c6.8,0.2,13.4,0.4,20,0.7"
-  +    "c6.5,0.3,12.9,0.7,19.1,1.2c12.4,0.9,24.1,2.1,34.9,3.5c10.8,1.5,20.6,3.1,29.3,4.8c8.7,1.7,16.2,3.5,22.4,5.2"
-  +    "c3.1,0.8,5.8,1.7,8.2,2.4c2.4,0.8,4.4,1.4,6,1.9C478.3,145.9,480,146.5,480,146.5z"
+  var swishString = "m958,47.174446c0,0 -3.763794,-0.836388 -10.664063,-2.300102c-3.554626,-0.83638 -7.736633,-1.463684 -12.755066,-2.509178c-5.018372,-0.83638 -10.873108,-1.881874 -17.355164,-2.927368c-12.964172,-2.300102 -28.855591,-4.181984 -47.047241,-6.272972c-18.19165,-2.090988 -38.892395,-3.763779 -61.26593,-5.018387c-22.373596,-1.254574 -46.838196,-2.090988 -72.557312,-2.718262c-12.964172,-0.20911 -26.13739,-0.20911 -39.728821,-0.20911c-13.59137,0 -27.391907,0.20911 -41.401489,0.41819c-28.019348,0.627304 -57.084045,1.463688 -86.567017,3.136486c-29.482849,1.463684 -59.384033,3.345589 -89.285156,5.64566c-15.055145,1.254608 -29.901123,2.509209 -44.747162,3.763786c-14.846008,1.254608 -29.692017,2.718292 -44.538055,4.181976c-7.318451,0.836411 -14.636932,1.463684 -21.955383,2.300102c-7.318451,0.83638 -14.427795,1.463684 -21.746277,2.300064c-7.318451,0.836418 -14.427795,1.672798 -21.53717,2.509216c-7.109344,0.83638 -14.218719,1.67276 -21.328094,2.300064c-28.228333,2.718292 -55.620285,4.80928 -81.548538,5.64566c-12.964111,0.418228 -25.719162,0.836418 -37.846878,0.836418c-12.336838,0.209076 -24.046371,0 -35.337708,-0.209114c-22.791779,-0.627304 -43.492569,-2.090988 -61.893257,-3.972862c-18.400703,-2.300102 -34.501308,-4.80928 -47.465439,-8.154877c-6.482067,-1.672768 -12.336834,-3.345558 -17.355202,-5.018356c-5.018375,-1.672798 -9.200352,-3.554672 -12.545934,-5.018364c-3.345582,-1.672798 -5.854766,-2.927399 -7.31846,-3.972893c-1.672789,-1.045494 -2.509184,-1.672798 -2.509184,-1.672798s0.836394,0.418221 2.718283,1.045494c1.672789,0.627304 4.391077,1.463715 7.736659,2.509209c3.345578,0.836388 7.736655,1.881882 12.755027,2.509186c5.018375,0.83638 10.873142,1.463684 17.355202,2.090988c6.482067,0.41819 13.800529,0.83638 21.537186,1.045494c7.945759,0.209076 16.309711,0.209076 25.510052,0.209076c18.191612,-0.209076 38.89238,-1.045494 61.265961,-2.300064c11.291351,-0.627304 22.791779,-1.463692 34.91951,-2.509186c12.127731,-1.045494 24.67366,-2.090988 37.428696,-3.136482c25.719162,-2.300095 52.692902,-5.22747 80.712158,-8.573063c14.009613,-1.672798 28.228333,-3.345592 42.656158,-5.22747c14.427795,-1.672798 29.273834,-3.345596 43.910767,-4.60017c14.846008,-1.463684 29.692017,-2.718288 44.747131,-3.972897c15.055145,-1.254574 30.11026,-2.300068 45.165344,-3.136467c30.110229,-1.881893 60.429535,-3.136482 90.121552,-3.763783c29.692078,-0.627289 58.965881,-0.627289 87.403381,0c14.218689,0.418205 28.019226,0.836395 41.819763,1.463699c13.591431,0.627289 26.973694,1.463684 39.937866,2.509178c25.928284,1.881893 50.392761,4.39109 72.975464,7.318462c22.582764,3.136482 43.074402,6.482075 61.266052,10.036751c18.191528,3.554672 33.873962,7.318455 46.838013,10.873135c6.482117,1.672798 12.127808,3.554672 17.146179,5.018356c5.018311,1.672798 9.200317,2.927406 12.545898,3.9729c6.272949,2.300064 9.827698,3.554672 9.827698,3.554672z"
 
   // console.log(swishString)
   var defaultSwishX = -21.700031280517578
@@ -163,13 +165,17 @@ function ready(error, raw) {
   .attr("class","big-swish")
   // .attr("y",100)
   .attr("d",swishString)
-  .attr("transform"," translate(-55,"+0+") scale("+scaleTo960+","+scaleTo960+")")
+  // .attr("transform"," translate(-55,"+0+") scale("+scaleTo960+","+scaleTo960+")")
+  .attr("transform"," translate(0,"+swishY+")")
   .attr("fill","url(#solids)")
 
   // console.log(bigSwish.getBBox())
   var bbox = d3.select(".big-swish")[0][0].getBBox()
   console.log(bbox)
-
+  var timeline = {
+      y: swishY + 40,
+      height: 15
+    }
   // svg.append(swish)
 
   // svg.append('rect')
@@ -200,13 +206,15 @@ function ready(error, raw) {
     .enter()
     .append('g')
 
-  items.append('rect')
-    .attr('x', function(d) {
-      return Global.xScale(d.dateValue)
+  items.append('circle')
+  // items.append('rect')
+    .attr('cx', function(d) {
+      return margin.left + Global.xScale(d.dateValue)
     })
-    .attr('y', timeline.y)
-    .attr('height', timeline.height)
-    .attr('width', 2)
+    .attr('cy', timeline.y)
+    .attr('r', 5)
+    // .attr('height', timeline.height)
+    // .attr('width', 2)
     // .attr('r',10)
     .on("mouseover", itemMouseover)
     .on("mouseout", itemMouseout)
@@ -219,7 +227,8 @@ function ready(error, raw) {
     .text("")
 
 
-
+  modifyBernieLink()
+  addRedditLink()
 }
 
 
@@ -229,14 +238,14 @@ function itemMouseover(d, i) {
   d3.select(".large-date").text(d.date)
   d3.select(".large-date").transition().duration(750).attr('opacity', 1)
 
-  var scaleFactor = 1.45
+  var scaleFactor = 1.19
   var translateFactor = (scaleFactor - 1) * -470
 
-  var howFarDownToShift = 100 - (i * 27)
+  var howFarDownToShift = 0- (i *23)
   d3.select('.text-group')
     .transition().duration(750)
-    // .attr("transform","translate("+translateFactor+","+howFarDownToShift+") scale("+scaleFactor+","+scaleFactor+")")
-    .attr("transform", "translate(0," + howFarDownToShift + ") scale(1," + scaleFactor + ")")
+    .attr("transform","translate("+translateFactor+","+howFarDownToShift+") scale("+scaleFactor+","+scaleFactor+")")
+    // .attr("transform", "translate(0," + howFarDownToShift + ") scale(1," + scaleFactor + ")")
 
 
   d3.selectAll('.blurb-tspan').attr('fill', '#DFDFDF')
@@ -254,8 +263,8 @@ function itemMouseover(d, i) {
 function itemMouseout(d, i) {
   var dur = 1500
 
-  d3.select(".large-date").transition().duration(dur).attr('opacity', 0)
-  d3.select('.text-group').transition().duration(dur).attr("transform", "translate(0,0) scale(1,1)")
+  d3.select(".large-date").transition().duration(dur).delay(500).attr('opacity', 0)
+  d3.select('.text-group').transition().duration(dur).delay(500).attr("transform", "translate(0,0) scale(1,1)")
 
   // d3.selectAll('.blurb-tspan').transition()
 
@@ -264,7 +273,7 @@ function itemMouseout(d, i) {
   // .attr('fill',"blue")
 
   d3.selectAll('.blurb-tspan')
-    .transition().duration(dur)
+    .transition().delay(500).duration(dur)
     // .attr('opacity',.5)
     .attr('fill', "#DFDFDF")
     // d3.select('.text-group').transition().attr("transform","scale(1,1)")
